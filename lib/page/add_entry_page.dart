69db8ad9d810882973f16b4dc378b4b30712c504
Piotr Merski <merskip@gmail.com>
2019-05-19 10:52:31 +0200

    Adding date form item do add entry page

--- lib/page/add_entry_page.dart
+++ lib/page/add_entry_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:home_budget/model/budget_configuration.dart';
 import 'package:home_budget/model/entry_metadata.dart';
+import 'package:intl/intl.dart';
 
 class AddEntryPage extends StatefulWidget {
 
@@ -38,23 +39,24 @@ class AddEntryState extends State<AddEntryPage> {
           itemCount: widget.budgetConfiguration.entryMetadata.cellsMetadata.length,
           itemBuilder: (BuildContext context, int index) {
             final cellMetadata = widget.budgetConfiguration.entryMetadata.cellsMetadata[index.toString()];
-            return _buildFormItem(cellMetadata);
+            return _buildFormItem(context, cellMetadata);
           },
           separatorBuilder: (BuildContext context, int index) => SizedBox(height: 16)
         )
       )
     );
 
-  _buildFormItem(CellMetadata cellMetadata) {
+  _buildFormItem(BuildContext context, CellMetadata cellMetadata) {
     if (cellMetadata.valueValidation == ValueValidation.none) {
       switch (cellMetadata.displayType) {
         case DisplayType.text:
         case DisplayType.title:
-        case DisplayType.date:
-        case DisplayType.category:
-          return _buildTextFormItem(cellMetadata);
         case DisplayType.amount:
           return _buildAmountFormItem(cellMetadata);
+        case DisplayType.date:
+          return _buildDateFormItem(context, cellMetadata);
+        case DisplayType.category:
+          return _buildTextFormItem(cellMetadata);
       }
     }
     else if (cellMetadata.valueValidation == ValueValidation.oneOfList) {
@@ -73,11 +75,29 @@ class AddEntryState extends State<AddEntryPage> {
   _buildAmountFormItem(CellMetadata cellMetadata) =>
     TextFormField(
       decoration: InputDecoration(
-        labelText: "Amount",
+        labelText: cellMetadata.title,
         border: OutlineInputBorder(),
         suffixText: "zł",
       ),
       keyboardType: TextInputType.numberWithOptions(signed: false, decimal: true)
+    );
+
+  _buildDateFormItem(BuildContext context, CellMetadata cellMetadata) =>
+    InkWell(
+      onTap: () async {
+        final monthBeforeNowDate = DateTime.now().add(Duration(days: -30));
+        final monthAfterNowDate = DateTime.now().add(Duration(days: 30));
+        final selectedDate = await showDatePicker(context: context, initialDate: DateTime.now(), firstDate: monthBeforeNowDate, lastDate: monthAfterNowDate);
+        print(selectedDate);
+      },
+
+      child: AbsorbPointer(child: TextFormField(
+        decoration: InputDecoration(
+          labelText: cellMetadata.title,
+          border: OutlineInputBorder(),
+        ),
+        initialValue: DateFormat(cellMetadata.dateFormat).format(DateTime.now()),
+      ))
     );
 
   _buildComboBoxFormItem(CellMetadata cellMetadata) {
@@ -93,7 +113,7 @@ class AddEntryState extends State<AddEntryPage> {
     );
   }
 
-    Widget _horizontalRadioGroup<T>(T groupValue, int valueCount, String Function(int index) valueBuilder) =>
+  Widget _horizontalRadioGroup<T>(T groupValue, int valueCount, String Function(int index) valueBuilder) =>
     SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       physics: BouncingScrollPhysics(),
