2eb3de5d10a7b306e4f0e2cfcf9d90335d337d0d
Piotr Merski <merskip@gmail.com>
2019-06-12 16:03:07 +0200

    Improvements for auto sign in

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,8 @@ import 'package:googleapis/sheets/v4.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'data/application_config.dart';
-import 'data/budget_sheet_config.dart';
-import 'screens/budget_sheet_config_screen.dart';
 import 'screens/budget_show_screen.dart';
 import 'screens/first_configuration_screen.dart';
-import 'screens/sign_in_screen.dart';
 import 'util/google_http_client.dart';
 
 const prefsApplicationConfig = "appliaction_config";
@@ -27,19 +24,14 @@ GoogleHttpClient httpClient;
 void main() => runApp(AppWidget());
 
 class AppWidget extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => AppState();
 }
 
 class AppState extends State<AppWidget> {
-
-  var showSignIn = true;
-
-  GoogleSignInAccount account;
-
-  var loading = false;
+  var loading = true;
   ApplicationConfig applicationConfig;
+  GoogleSignInAccount account;
 
   @override
   void initState() {
@@ -48,22 +40,41 @@ class AppState extends State<AppWidget> {
   }
 
   _startupApplication() async {
-    final applicationConfig = await _readApplicationConfigFromPreferencesOrNull();
+    this.applicationConfig = await _readApplicationConfigFromPreferencesOrNull();
+
+    if (applicationConfig != null) {
+      this.account = await _trySignInSilently();
+
+      while (this.account == null) {
+        this.account = await googleSignIn.signIn();
+      }
+
+      if (this.account != null) {
+        httpHeaders = await googleSignIn.currentUser.authHeaders;
+        httpClient = GoogleHttpClient(httpHeaders);
+      }
+    }
 
     setState(() {
-      this.applicationConfig = applicationConfig;
       this.loading = false;
     });
+  }
+
+  Future<GoogleSignInAccount> _trySignInSilently() async {
+    try {
+      return await googleSignIn.signInSilently();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 
   Future<ApplicationConfig> _readApplicationConfigFromPreferencesOrNull() async {
     final preferences = await SharedPreferences.getInstance();
     try {
       final json = preferences.getString(prefsApplicationConfig);
-      if (json != null)
-        return ApplicationConfig.fromJson(jsonDecode(json));
-    }
-    catch (e) {
+      if (json != null) return ApplicationConfig.fromJson(jsonDecode(json));
+    } catch (e) {
       print(e);
       preferences.remove(prefsApplicationConfig);
     }
@@ -71,35 +82,32 @@ class AppState extends State<AppWidget> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) =>
+    MaterialApp(
       title: 'Home Budget',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         buttonTheme: ButtonThemeData(
           buttonColor: Colors.indigo,
-          textTheme: ButtonTextTheme.primary
-        )
+          textTheme: ButtonTextTheme.primary,
+        ),
       ),
-      home: Builder(builder: (context) {
-        if (loading)
-          _loadingScreen();
-        else if (applicationConfig == null)
-          return FirstConfigurationScreen(() {
-            _startupApplication();
-          });
-        else
-          return _budgetShowScreen(applicationConfig);
-
-      })
+      home: Builder(
+        builder: (context) {
+          if (loading)
+            return _loadingScreen();
+          else if (applicationConfig == null)
+            return FirstConfigurationScreen(() => _startupApplication());
+          else
+            return _budgetShowScreen(applicationConfig);
+        },
+      ),
     );
-  }
 
   Widget _loadingScreen() =>
-    Scaffold(
-      body: Center(child: CircularProgressIndicator())
-    );
+    Scaffold(body: Center(child: CircularProgressIndicator()));
 
   Widget _budgetShowScreen(ApplicationConfig applicationConfig) =>
-    BudgetShowScreen(budgetSheetConfig: applicationConfig.defaultBudgetSheetConfig);
+    BudgetShowScreen(
+      budgetSheetConfig: applicationConfig.defaultBudgetSheetConfig);
 }
